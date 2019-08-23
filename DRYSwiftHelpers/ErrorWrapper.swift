43acafa56682197676c6221bca39648e9bba52a4
Yuri Drozdovsky <yuriy.drozdovsky@belprog.com>
2019-08-23 13:32:29 +0300

    bugfixing

--- DRYSwiftHelpers/ErrorWrapper.swift
+++ DRYSwiftHelpers/ErrorWrapper.swift
@@ -5,11 +5,25 @@
 
 import Foundation
 
-public class ErrorWrapper: Error, LocalizedError {
+public class ErrorWrapper: Error, CustomStringConvertible, LocalizedError {
+    fileprivate(set) var log = [String]()
     var innerError: Error
 
     public init(innerError: Error) {
         self.innerError = innerError
+    }
+
+    public var description: String {
+        get {
+            var result = ""
+            for s in log {
+                result.append(s)
+                if result.last != "\n" {
+                    result.append("\n")
+                }
+            }
+            return result
+        }
     }
 
     public var errorDescription: String? {
@@ -19,15 +33,19 @@ public class ErrorWrapper: Error, LocalizedError {
     }
 }
 
-public func wrapError<T>(_ routine: @autoclosure () throws -> T) throws -> T {
+public func wrapError<T>(_ routine: @autoclosure () throws -> T, file: String = #file, line: UInt = #line, column: UInt = #column) throws -> T {
     do {
         return try routine()
     } catch {
-        if let extendedError = error as? ErrorWrapper {
-            throw extendedError
+        if let wrappedError = error as? ErrorWrapper {
+            let fileName = URL(fileURLWithPath: file, isDirectory: false).lastPathComponent
+            wrappedError.log.insert("\(fileName):\(line):\(column)", at: 0)
+            throw wrappedError
         } else {
-            let extendedError = ErrorWrapper(innerError: error)
-            throw extendedError
+            let wrappedError = ErrorWrapper(innerError: error)
+            let fileName = URL(fileURLWithPath: file, isDirectory: false).lastPathComponent
+            wrappedError.log.append("\(fileName):\(line):\(column) \(String(describing: error)))")
+            throw wrappedError
         }
     }
 }
